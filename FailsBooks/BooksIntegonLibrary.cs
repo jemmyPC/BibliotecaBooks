8d5117071e19e37b90aefaca730c991d8383417e
JEMMY MARISOL PRECIADO CALDERON <preciadojemmy@gmail.com>
2020-01-17 02:46:08 -0800

    terinada

--- FailsBooks/BooksIntegonLibrary.cs
+++ FailsBooks/BooksIntegonLibrary.cs
@@ -20,14 +20,19 @@ namespace DebsExec
         {
             try
             {
-                var loans = db.Loan.Where(d => d.DateCreate.Value.AddDays(5) >= DateTime.Now && d.DateFinish == null);
+                var loans = db.Loan.Where(d => d.DateFinish == null).ToList();
                 foreach (var loan in loans)
                 {
-                    loan.StatusId = 4;
-                    loan.Price = 10 * ((int)(loan.DateCreate.Value.AddDays(5) - DateTime.Now).TotalDays);
-                    db.Loan.Add(loan);
-                    db.Entry(loan).State = System.Data.Entity.EntityState.Modified;
-                    db.SaveChanges();
+                    int diff = (DateTime.Now - loan.DateCreate).Value.Days;
+                    if(diff > 5)
+                    {
+                        loan.StatusId = 4;
+                        loan.Price = 10 * diff;
+                        db.Loan.Add(loan);
+                        db.Entry(loan).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                    }
+
                 }
                 
 
